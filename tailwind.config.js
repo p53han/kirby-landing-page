e0ced919d4d0a8efe2ae0ab8978222485eef0fab
p53han <patrickgrogor@gmail.com>
2025-04-27 12:47:18 +0200

    Installiere Kirby Starter Kit und Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './site/templates/**/*.php',
+    './site/snippets/**/*.php',
+    './site/controllers/**/*.php',
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
